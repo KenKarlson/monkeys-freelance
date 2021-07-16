79b065b7b3ebf37dcfa05659e9f5aeb72f9a4d72
Гусь Гитарист <54804808+KenKarlson@users.noreply.github.com>
2021-07-16 23:20:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,96 @@
+document.addEventListener('DOMContentLoaded', () =>{
+  'use strict';
+
+  const customer = document.getElementById('customer'),
+      freelancer = document.getElementById('freelancer'),
+      blockCustomer = document.getElementById('block-customer'),
+      blockFreelancer = document.getElementById('block-freelancer'),
+      blockChoice = document.getElementById('block-choice'),
+      btnExit = document.getElementById('btn-exit'),
+      formCustomer = document.getElementById('form-customer'),
+      ordersTable = document.getElementById('orders'),
+      modalOrder = document.getElementById('order_read'),
+      modalOrderActive = document.getElementById('order_active');
+
+  const orders = [];
+  //const wwwwwww = [modalOrder, modalOrderActive];
+
+  const renderOrders = () => {
+    ordersTable.textContent = '';
+    orders.forEach((order, i) => {          
+        ordersTable.innerHTML += `
+            <tr class="order" data-number-order="${i}">
+                <td>${i+1}</td>
+                <td>${order.title}</td>
+                <td class="${order.currency}"></td>
+                <td>${order.deadline}</td>
+            </tr>`;
+
+      }) ;
+  };
+
+  const openModal = (numberOrder) => {
+      const order = orders[numberOrder];
+      const modal = !order.active ? modalOrderActive : modalOrder;
+      modal.style.display = 'block';
+  };
+
+  ordersTable.addEventListener('click', (event) =>{
+      const target = event.target;
+      
+      const targetOrder = target.closest('.order')
+        if(targetOrder){
+          openModal(targetOrder.dataset.numberOrder);
+        }
+
+      
+  });
+
+
+  customer.addEventListener('click' , () =>{
+    blockChoice.style.display = 'none';
+    blockCustomer.style.display = 'block';
+    btnExit.style.display = 'block';
+  });
+
+  freelancer.addEventListener('click' , () =>{
+    blockChoice.style.display = 'none';
+    renderOrders();
+    blockFreelancer.style.display = 'block';
+    btnExit.style.display = 'block';
+  });
+
+  btnExit.addEventListener('click' , () =>{
+    btnExit.style.display = 'none';
+    blockFreelancer.style.display = 'none';
+    blockCustomer.style.display = 'none';
+    blockChoice.style.display = 'block';
+    
+  });
+
+
+  formCustomer.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const obj = {};
+    
+    [...formCustomer.elements].forEach((elem) => {
+      
+      if((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+      (elem.type === 'radio' && elem.checked) ||
+      elem.tagName === 'TEXTAREA'){
+        obj[elem.name] = elem.value;
+        if(elem.type !== 'radio'){
+            elem.value = '';
+        }
+      }
+    });
+
+    formCustomer.reset();
+
+    orders.push(obj);
+    
+  }); 
+
+
+})
