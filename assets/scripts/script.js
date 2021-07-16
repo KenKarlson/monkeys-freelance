cdadefef64e06257ba33d952744f7b4a76fce730
Гусь Гитарист <54804808+KenKarlson@users.noreply.github.com>
2021-07-16 22:51:50 +0300

    Add files via upload

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,15 +1,49 @@
 document.addEventListener('DOMContentLoaded', () =>{
   'use strict';
-  const customer = document.getElementById('customer');
-  const freelancer = document.getElementById('freelancer');
 
-  const blockCustomer = document.getElementById('block-customer');
-  const blockFreelancer = document.getElementById('block-freelancer');
-  const blockChoice = document.getElementById('block-choice');
-  const btnExit = document.getElementById('btn-exit');
-  const formCustomer = document.getElementById('form-customer');
+  const customer = document.getElementById('customer'),
+      freelancer = document.getElementById('freelancer'),
+      blockCustomer = document.getElementById('block-customer'),
+      blockFreelancer = document.getElementById('block-freelancer'),
+      blockChoice = document.getElementById('block-choice'),
+      btnExit = document.getElementById('btn-exit'),
+      formCustomer = document.getElementById('form-customer'),
+      ordersTable = document.getElementById('orders')
+      modalOrder = document.getElementById('order_read'),
+      modalOrderActive = document.getElementById('order_active');
 
   const orders = [];
+
+  const renderOrders = () => {
+    ordersTable.textContent = '';
+    orders.forEach((order, i) => {          
+        ordersTable.innerHTML += `
+            <tr class="order taken" data-number-order="${i}">
+                <td>${i+1}</td>
+                <td>${order.title}</td>
+                <td class="${order.currency}"></td>
+                <td>${order.deadline}</td>
+            </tr>`;
+
+      }) ;
+  };
+
+  const openModal = (numberOrder) => {
+      const order = orders[numberOrders];
+      const modal = !order.active ? modalOrderActive : modalOrder;
+      modal.style.display = 'block';
+  };
+
+  ordersTable.addEventListener('click', (event) =>{
+      const target = event.target;
+      
+      const targetOrder = target.closest('.order')
+        if(targetOrder){
+          openModal(targetOrder.dataset.numberOrder);
+        }
+
+      
+  });
 
 
   customer.addEventListener('click' , () =>{
@@ -20,6 +54,7 @@ document.addEventListener('DOMContentLoaded', () =>{
 
   freelancer.addEventListener('click' , () =>{
     blockChoice.style.display = 'none';
+    renderOrders();
     blockFreelancer.style.display = 'block';
     btnExit.style.display = 'block';
   });
@@ -32,23 +67,29 @@ document.addEventListener('DOMContentLoaded', () =>{
     
   });
 
-  formCustomer.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const obj = {}
-    for(const elem of formCustomer.elements){
+
+  formCustomer.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const obj = {};
+    
+    [...formCustomer.elements].forEach((elem) => {
+      
       if((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
       (elem.type === 'radio' && elem.checked) ||
       elem.tagName === 'TEXTAREA'){
-        
         obj[elem.name] = elem.value;
-
         if(elem.type !== 'radio'){
-          elem.value = '';
+            elem.value = '';
         }
-      }      
-    }
+      }
+    });
+
+    formCustomer.reset();
+
     orders.push(obj);
-  });
+    
+  }); 
 
 
 })
