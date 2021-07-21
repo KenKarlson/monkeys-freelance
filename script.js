2779a810aeac7e393ce3fa2d6a14a059883526e1
Гусь Гитарист <54804808+KenKarlson@users.noreply.github.com>
2021-07-21 17:29:07 +0300

    Final?

--- script.js
+++ script.js
@@ -12,17 +12,40 @@ document.addEventListener('DOMContentLoaded', () =>{
       modalOrder = document.getElementById('order_read'),
       modalOrderActive = document.getElementById('order_active');
 
-  const orders = [];
+      //localStorage.clear()
+
+
+  const orders = JSON.parse(localStorage.getItem('freeOrders')) || [];
+
+  const toStorage = () => {
+    localStorage.setItem('freeOrders', JSON.stringify(orders));
+  };
+  const declOfNum = (number, titles) => number + ' ' + titles[(number % 100 > 4 && number % 100 < 20) ? 
+    2 : [2, 0, 1, 1, 1, 2][(number % 10 < 5) ? number % 10 : 5]];
+
+  const calcDeadline = (data) => {
+    const dateDeadline = new Date(data);
+    const toDay = Date.now();
+    const remaining = ((dateDeadline - toDay) / 1000 / 60 / 60);
+    if(remaining / 24 > 2){
+      return declOfNum(Math.floor(remaining / 24), ['день','дня', 'дней']);
+    }
+      return declOfNum(Math.floor(remaining), ['час','часа', 'часов']);
+    
+  };
+
+  
 
   const renderOrders = () => {
     ordersTable.textContent = '';
     orders.forEach((order, i) => {          
         ordersTable.innerHTML += `
-            <tr class="order" data-number-order="${i}">
+            <tr class="order ${order.active ? 'taken' : ''}" 
+                data-number-order="${i}">
                 <td>${i+1}</td>
                 <td>${order.title}</td>
                 <td class="${order.currency}"></td>
-                <td>${order.deadline}</td>
+                <td>${calcDeadline(order.deadline)}</td>
             </tr>`;
 
       }) ;
@@ -31,8 +54,36 @@ document.addEventListener('DOMContentLoaded', () =>{
   const handlerModal = (event) =>{
     const target = event.target;
     const modal = target.closest('.order-modal');
+    const order = orders[modal.id];
 
-    console.log(modal, modal.id);
+
+    //Orders to change 
+    const baseAction = () =>{
+      modal.style.display = 'none';
+      toStorage();
+      renderOrders();
+    };
+
+    if(target.closest('.close') || target == modal){
+        modal.style.display = 'none';
+        
+    };
+
+    if(target.classList.contains('get-order')){
+      order.active = true; 
+      baseAction();         
+    };
+
+    if(target.id === 'capitulation'){
+      order.active = false;
+      baseAction();      
+    };
+
+    if(target.id === 'ready'){      
+      orders.splice(orders.indexOf(order),1); 
+      baseAction();     
+    };
+
   };
 
   const openModal = (numberOrder) => {
@@ -52,16 +103,17 @@ document.addEventListener('DOMContentLoaded', () =>{
           phoneBlock = modal.querySelector('.phone');
 
           modal.id = numberOrder;
+
       titleBlock.textContent = title;
       firstNameBlock.textContent = firstName;
       emailBlock.textContent = email;
       emailBlock.href = 'mail to:' + email;
       descriptionBlock.textContent = description;
-      deadlineBlock.textContent = deadline;
+      deadlineBlock.textContent = calcDeadline(deadline);
       currencyBlock.className = 'currency_img';
       currencyBlock.classList.add(currency); 
       countBlock.textContent = ' x ' +  amount;
-      phoneBlock ? phoneBlock.href = 'tel:' + phone : '';
+      phoneBlock && (phoneBlock.href = 'tel:' + phone);
 
       modal.style.display = 'flex';
 
@@ -82,6 +134,15 @@ document.addEventListener('DOMContentLoaded', () =>{
 
   customer.addEventListener('click' , () =>{
     blockChoice.style.display = 'none';
+    
+    var d = new Date();
+        d.setDate(d.getDate() + 1);
+        const toDay1 = d;
+    //alert( toDay1.toISOString().substring(0, 10) );
+    const toDay = new Date().toISOString().substring(0, 10);
+    
+    document.getElementById('deadline').min = toDay1.toISOString().substring(0, 10);
+    //document.getElementById('deadline').min = toDay;
     blockCustomer.style.display = 'block';
     btnExit.style.display = 'block';
   });
@@ -89,6 +150,7 @@ document.addEventListener('DOMContentLoaded', () =>{
   freelancer.addEventListener('click' , () =>{
     blockChoice.style.display = 'none';
     renderOrders();
+    
     blockFreelancer.style.display = 'block';
     btnExit.style.display = 'block';
   });
@@ -122,6 +184,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     formCustomer.reset();
 
     orders.push(obj);
+    toStorage();
     
   }); 
 
